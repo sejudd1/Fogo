78e36a665860e64b6fd939b446c6879bd05aae76
PercivalJ Loyola <percivalloyal@gmail.com>
2015-10-05 16:02:37 -0700

    Adding User Controllers,Routes,Linking accounts

---
+++ config/routes/userRoutes.js
@@ -0,0 +1,129 @@
+module.exports = function(app, passport) {
+
+
+
+	// Home page - Log in Sign up will be visible
+	app.get('/', function(req, res) {
+		res.render('layout.ejs')
+	})
+
+	// User Profile
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user
+		})
+	})
+
+	// Logout
+	app.get('/logout', function(req, res) {
+		req.logout()
+		res.redirect('/')
+	})
+
+// ==========================
+// AUTHENTICATE FIRST LOGIN 
+// ==========================
+
+	//LOCAL LOG IN
+	app.get('/login', function(req, res) {
+		res.render('login.ejs', { message: req.flash('loginMessage') })
+	})	
+	//process the login form
+	app.post('/login', passport.authenticate('local-login', {
+		// redirect to the secure profile section
+		successRedirect : '/profile', 
+		// redirect back to the signup page if there is an error
+		failureRedirect : '/login', 
+		failureFlash : true 
+	}))
+
+	// SIGNUP ===
+
+	// show the signup form
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('loginMessage') })
+	})
+
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		// redirect to the secure profile section
+		successRedirect : '/profile', 
+		// redirect back to the signup page if there is an error
+		failureRedirect : '/signup', 
+		failureFlash : true 
+	}))
+
+	//FACEBOOK LOG IN ===
+
+	// send to facebook to do the authentication
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }))
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+		successRedirect : '/profile',
+		failureRedirect : '/'
+	}))
+
+	
+// ===============================================================
+// AUTHORIZE already logged in and Connect/Link to Facebook
+
+	// LOCAL
+	app.get('/connect/local', function(req, res) {
+		res.render('connect-local.ejs', { message: req.flash('loginMessage') })
+	})
+	app.post('/connect/local', passport.authenticate('local-signup', {
+		// redirect to the secure profile section
+		successRedirect : '/profile', 
+		// redirect back to the signup page if there is an error
+		failureRedirect : '/connect/local', 
+		failureFlash : true
+	}))
+
+	// FACEBOOK
+
+	// send to facebook to do the authentication
+	app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }))
+
+	// handle the callback after facebook has authorized the user
+	app.get('/connect/facebook/callback',
+		passport.authorize('facebook', {
+		successRedirect : '/profile',
+		failureRedirect : '/'
+	}))
+// ================
+// UNLINK ACCOUNTS 
+// ================
+// user account will stay active in case they want to reconnect in the future
+
+	// LOCAL  - remove email and password
+	app.get('/unlink/local', function(req, res) {
+		var user            = req.user
+		user.local.email    = undefined
+		user.local.password = undefined
+		user.save(function(err) {
+			res.redirect('/profile')
+		})
+	})
+
+	// FACEBOOK - remove token
+	app.get('/unlink/facebook', function(req, res) {
+		var user            = req.user
+		user.facebook.token = undefined
+		user.save(function(err) {
+			res.redirect('/profile')
+		})
+	})
+
+}
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		return next()
+
+	res.redirect('/')
+}
+
+}
--- models/User.js
+++ models/User.js
@@ -1,13 +1,20 @@
 //===================
 //DECLARING VARIABLES
 //===================
-var mongoose = require( 'mongoose' )
+var mongoose 		= require( 'mongoose' )
+var bcrypt 			= require( 'bcrypt-nodejs' )
 
 //===============
 //CREATING SCHEMA
 //===============
 
-module.exports = mongoose.model( 'User', {
+//All user accounts setup within their own object. To link and unlink user accounts.
+var userSchema = mongoose.Schema({
+	local:{
+		email: String,
+		password: String,
+	},
+	
 	fb:{
 		id: String,
 		access_token: String,
@@ -15,7 +22,16 @@ module.exports = mongoose.model( 'User', {
 		lastName: String,
 		email: String
 
-
 	}
 
 })
+//Validates User Password
+User.methods.validatePassword = function ( password ) {
+  return bcrypt.compareSync( password, this.local.password )
+}
+//Adds hash to password 
+User.methods.encrypt = function( password ) {
+  return bcrypt.hashSync( password, bcrypt.genSaltSync( 8 ), null )
+}
+//Exports Model to the app
+module.exports = mongoose.model( 'User', User )
