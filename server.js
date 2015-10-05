bc6b2f827b57b695ee9d40124c3b9e37dff890a6
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-10-05 09:45:26 -0700

    Adds test route and console.log shortcut

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+//console.log function
+function c( word ) {
+	console.log( word )
+}
+
+
 //===================
 //DECLARING VARIABLES
 //===================
@@ -7,11 +13,20 @@ var port    = process.env.PORT || 3000
 //var DB      = process.env.DATABASE_URL || //insert database string for local
 
 
-
 //===================
 //ROUTES
 //===================
 
+//test route for home
+app.get( '/', function( req, res ) {
+	c( 'this works' )
+	res.json( "Hello" )
+} )
+
+
 //===============
 //SERVER PORT
 //==============
+app.listen( port, function() {
+	c( 'server running on port ' + port )
+} )
