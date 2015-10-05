90f72fccfa6c144183cda235f86aa2d92a7902c5
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-10-05 09:55:58 -0700

    Adds conversation model

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ function c( word ) {
 //===================
 //DECLARING VARIABLES
 //===================
-var express = require( 'express' )
-var app     = express()
-var port    = process.env.PORT || 3000
-//var DB      = process.env.DATABASE_URL || //insert database string for local
+var express  = require( 'express' )
+var app      = express()
+var mongoose = require( 'mongoose' )
+var port     = process.env.PORT || 3000
+var DB       = process.env.DATABASE_URL || 'mongodb://localhost:27017/fogo'
 
+//Connect to database
+mongoose.connect( DB )
 
 //===================
 //ROUTES
