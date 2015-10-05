990fab23d2e13ed65f12a96ecd08aad5d98711ee
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-10-05 09:38:54 -0700

    Adds starter files/folders

---
+++ server.js
@@ -0,0 +1,17 @@
+//===================
+//DECLARING VARIABLES
+//===================
+var express = require( 'express' )
+var app     = express()
+var port    = process.env.PORT || 3000
+//var DB      = process.env.DATABASE_URL || //insert database string for local
+
+
+
+//===================
+//ROUTES
+//===================
+
+//===============
+//SERVER PORT
+//==============
