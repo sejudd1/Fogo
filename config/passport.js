d271771c4d6351b30bd99c5e72a533115fb7617b
Steve Judd <sejudd1@github.com>
2015-10-05 12:35:28 -0700

    removes double quotes, adds single quotes

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
+//===========================
 //This is our passport module
-var User = require( "../models/User" )
-	FacebookStrategy = require( "passport-facebook" ).Strategy
+//===========================
+var User = require( '../models/User' )
+	FacebookStrategy = require( 'passport-facebook' ).Strategy
 
 module.exports = function( passport ){
 	//Get user id to store for session
@@ -15,19 +17,19 @@ module.exports = function( passport ){
 		})
 	})
 
-	passport.use( "facebook", new FacebookStrategy({
+	passport.use( 'facebook', new FacebookStrategy({
         clientID        : process.env.FACEBOOK_API_KEY,
         clientSecret    : process.env.FACEBOOK_API_SECRET,
-        callbackURL     : "http://localhost:3000/auth/facebook/callback",
+        callbackURL     : 'http://localhost:3000/auth/facebook/callback',
         enableProof     : true,
-        profileFields   : [ "id", "name", "email", "photos" ]
+        profileFields   : [ 'id', 'name', 'email', 'photos' ]
     }, function( access_token, refresh_token, profile, done ) {
 
     // Use this to see the information returned from Facebook
 
         process.nextTick( function() {
 
-            User.findOne({ "fb.id" : profile.id }, function( err, user ) {
+            User.findOne({ 'fb.id' : profile.id }, function( err, user ) {
                 if ( err ) {
                     return done( err );
                 } else if ( user ) {
