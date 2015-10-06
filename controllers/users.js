01cdb9a7e3365a73bf26bc690ef6bfd1df927b2b
PercivalJ Loyola <percivalloyal@gmail.com>
2015-10-05 18:34:48 -0700

    adhering to code style

--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,7 @@ var User = require( '../models/user.js' )
 		} )
 	}
 
-	function create(req, res){
+	function create( req, res ){
 		// make a single user -- create
 		var user = new User()
 
@@ -28,40 +28,40 @@ var User = require( '../models/user.js' )
 		user.username = req.body.username
 		user.password = req.body.password
 
-		user.save(function(err){
-			if(err){
-				if(err.code == 11000){
+		user.save(function( err ){
+			if( err ){
+				if( err.code == 11000 ){
 					return res.json({success: false, message: 'username already exists' })
 				} else {
-					res.send(err)
+					res.send( err )
 				}
 			}
 			res.json({success: true, message: 'Account created, Lets FOGO!'})
-		})
+		} )
 	}
 
-	function show(req, res){
+	function show( req, res ){
 		//get a single user -- show
-		User.findById(req.params.user_id, function(err, user){
-			if(err) res.send(err)
-			res.json(user)
-		})
+		User.findById(req.params.user_id, function( err, user ){
+			if( err ) res.send( err )
+			res.json( user )
+		} )
 	}
 
 	function update(req, res){
 		// update a single user -- update
-		User.findById(req.params.user_id, function(err, user){
-			if(err) res.send(err)
+		User.findById(req.params.user_id, function( err, user ){
+			if( err ) res.send( err )
 
 			if(req.body.name) user.name = req.body.name
 			if(req.body.username) user.username = req.body.username
 			if(req.body.password) user.password = req.body.password
 
-			user.save(function(err){
-				if(err) res.send(err)
-				res.json({success: true, message: 'Your FOGO account has been updated!'})
+			user.save(function( err ){
+				if( err ) res.send( err )
+				res.json({success: true, message: 'Your FOGO account has been updated!'} )
 			})
-		})
+		} )
 	}
 
 	function destroy(req, res){
@@ -71,7 +71,7 @@ var User = require( '../models/user.js' )
 		}, function(err, user){
 			if(err) res.send(err)
 			res.json({success: true, message: 'Your FOGO account has been Deleted'})
-		})
+		} )
 	}
 
 
