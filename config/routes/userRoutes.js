56b8a272dbd7431bab0ab8d7a0ff77db312d12da
Tali Walt <taliwalt@gmail.com>
2015-10-07 09:23:41 -0700

    Front-End

--- config/routes/userRoutes.js
+++ config/routes/userRoutes.js
@@ -1,127 +1,122 @@
-module.exports = function( app, passport ) {
+module.exports = function ( app, passport ) {
+  // Home page - Log in Sign up will be visible
+  app.get('/', function ( req, res ) {
+    res.render('layout.ejs')
+  })
 
+  // User Profile
+  app.get('/profile', isLoggedIn, function ( req, res ) {
+    res.render('profile.ejs', {
+      user: req.user
+    })
+  })
 
+  // Logout
+  app.get('/logout', function ( req, res ) {
+    req.logout()
+    res.redirect('/')
+  })
 
-	// Home page - Log in Sign up will be visible
-	app.get( '/', function( req, res ) {
-		res.render( 'layout.ejs' )
-	} )
+  // ==========================
+  // AUTHENTICATE FIRST LOGIN
+  // ==========================
 
-	// User Profile
-	app.get( '/profile', isLoggedIn, function( req, res ) {
-		res.render( 'profile.ejs', {
-			user : req.user
-		} )
-	} )
+  // LOCAL LOG IN
+  app.get('/login', function ( req, res ) {
+    res.render('login.ejs', { message: req.flash('loginMessage') })
+  })
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    // redirect to the secure profile section
+    successRedirect: '/profile',
+    // redirect back to the signup page if there is an error
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
 
-	// Logout
-	app.get( '/logout', function( req, res ) {
-		req.logout()
-		res.redirect( '/' )
-	} )
+  // SIGNUP ===
 
-// ==========================
-// AUTHENTICATE FIRST LOGIN 
-// ==========================
+  // show the signup form
+  app.get('/signup', function ( req, res ) {
+    res.render('signup.ejs', { message: req.flash('loginMessage') })
+  })
 
-	//LOCAL LOG IN
-	app.get( '/login', function( req, res ) {
-		res.render( 'login.ejs', { message: req.flash( 'loginMessage' ) } )
-	} )	
-	//process the login form
-	app.post( '/login', passport.authenticate('local-login', {
-		// redirect to the secure profile section
-		successRedirect : '/profile', 
-		// redirect back to the signup page if there is an error
-		failureRedirect : '/login', 
-		failureFlash : true 
-	} ) )
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    // redirect to the secure profile section
+    successRedirect: '/profile',
+    // redirect back to the signup page if there is an error
+    failureRedirect: '/signup',
+    failureFlash: true
+  }))
 
-	// SIGNUP ===
+  // FACEBOOK LOG IN ===
 
-	// show the signup form
-	app.get( '/signup', function( req, res ) {
-		res.render( 'signup.ejs', { message: req.flash( 'loginMessage' ) } )
-	} )
+  // send to facebook to do the authentication
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }))
 
-	// process the signup form
-	app.post( '/signup', passport.authenticate( 'local-signup', {
-		// redirect to the secure profile section
-		successRedirect : '/profile', 
-		// redirect back to the signup page if there is an error
-		failureRedirect : '/signup', 
-		failureFlash : true 
-	} ) )
+  // handle the callback after facebook has authenticated the user
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      successRedirect: '/profile',
+      failureRedirect: '/'
+    }))
 
-	//FACEBOOK LOG IN ===
+  // ===============================================================
+    // AUTHORIZE already logged in and Connect/Link to Facebook
 
-	// send to facebook to do the authentication
-	app.get( '/auth/facebook', passport.authenticate( 'facebook', { scope : 'email' } ) )
+  // LOCAL
+  app.get('/connect/local', function ( req, res ) {
+    res.render('connect-local.ejs', { message: req.flash('loginMessage') })
+  })
+  app.post('/connect/local', passport.authenticate('local-signup', {
+    // redirect to the secure profile section
+    successRedirect: '/profile',
+    // redirect back to the signup page if there is an error
+    failureRedirect: '/connect/local',
+    failureFlash: true
+  }))
 
-	// handle the callback after facebook has authenticated the user
-	app.get( '/auth/facebook/callback',
-		passport.authenticate( 'facebook', {
-		successRedirect : '/profile',
-		failureRedirect : '/'
-	} ) )
+  // FACEBOOK
 
-	
-// ===============================================================
-// AUTHORIZE already logged in and Connect/Link to Facebook
+  // send to facebook to do the authentication
+  app.get('/connect/facebook', passport.authorize('facebook', { scope: 'email' }))
 
-	// LOCAL
-	app.get( '/connect/local', function( req, res ) {
-		res.render( 'connect-local.ejs', { message: req.flash( 'loginMessage' ) } )
-	} )
-	app.post( '/connect/local', passport.authenticate( 'local-signup', {
-		// redirect to the secure profile section
-		successRedirect : '/profile', 
-		// redirect back to the signup page if there is an error
-		failureRedirect : '/connect/local', 
-		failureFlash : true
-	} ) )
+  // handle the callback after facebook has authorized the user
+  app.get('/connect/facebook/callback',
+    passport.authorize('facebook', {
+      successRedirect: '/profile',
+      failureRedirect: '/'
+    }))
+    // ================
+    // UNLINK ACCOUNTS
+    // ================
+    // user account will stay active in case they want to reconnect in the future
 
-	// FACEBOOK
+  // LOCAL  - remove email and password
+  app.get('/unlink/local', function ( req, res ) {
+    var user = req.user
+    user.local.email = undefined
+    user.local.password = undefined
+    user.save(function ( err ) {
+      res.redirect('/profile')
+    })
+  })
 
-	// send to facebook to do the authentication
-	app.get( '/connect/facebook', passport.authorize( 'facebook', { scope : 'email' } ) )
+  // FACEBOOK - remove token
+  app.get('/unlink/facebook', function ( req, res ) {
+    var user = req.user
+    user.facebook.token = undefined
+    user.save(function ( err ) {
+      res.redirect('/profile')
+    })
+  })
 
-	// handle the callback after facebook has authorized the user
-	app.get( '/connect/facebook/callback',
-		passport.authorize( 'facebook', {
-		successRedirect : '/profile',
-		failureRedirect : '/'
-	} ) )
-// ================
-// UNLINK ACCOUNTS 
-// ================
-// user account will stay active in case they want to reconnect in the future
+  // route middleware to ensure user is logged in
+  function isLoggedIn ( req, res, next ) {
+    if ( req.isAuthenticated())
+      return next()
 
-	// LOCAL  - remove email and password
-	app.get( '/unlink/local', function( req, res ) {
-		var user            = req.user
-		user.local.email    = undefined
-		user.local.password = undefined
-		user.save(function( err ) {
-			res.redirect( '/profile' )
-		} )
-	} )
-
-	// FACEBOOK - remove token
-	app.get( '/unlink/facebook', function( req, res ) {
-		var user            = req.user
-		user.facebook.token = undefined
-		user.save(function( err ) {
-			res.redirect( '/profile' )
-		} )
-	} )
-
-
-	// route middleware to ensure user is logged in
-	function isLoggedIn( req, res, next ) {
-		if ( req.isAuthenticated())
-			return next()
-	
-		res.redirect('/')
-	}
+    res.redirect('/')
+  }
 }
